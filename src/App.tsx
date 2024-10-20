aaaa077d1e9c8904477d6bcb9c2d8428ca1a2860
yoyobara <yotambarak6@gmail.com>
2024-10-21 02:00:00 +0300

    setup sass, finished reactjs setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import "./App.scss"
 
 function App() {
     return (
-        <div>hello!</div>
+        <Button variant='primary'>Click Me!</Button>
     )
 }
 
