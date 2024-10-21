a13da97359dd4c5825936cb5f00e2c7a0936167c
yoyobara <yotambarak6@gmail.com>
2024-10-21 16:28:54 +0300

    figured out alerts cards list

--- src/MyAlerts.tsx
+++ src/MyAlerts.tsx
@@ -1,6 +1,30 @@
+import { Card, Row } from "react-bootstrap";
+
+const RED_ALERT_URL = "https://api.tzevaadom.co.il/notifications";
+
 function MyAlerts() {
     return (
-        <div>hello!</div>
+        <Row className="d-flex">
+        </Row>
+    )
+}
+
+interface AlertProps {
+    cityName: string,
+    time: Date
+}
+
+function Alert({cityName, time}: AlertProps) {
+    return (
+        <div className="col-4 p-3"> 
+            <Card>
+                <Card.Img variant="top" src="/alert.jpg"/>
+                <Card.Body>
+                    <Card.Title>{cityName}</Card.Title>
+                    <Card.Subtitle>{time?.toTimeString()}</Card.Subtitle>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
